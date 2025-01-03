9af6c87786e27e5f159d105a6ede93a553ac55ac
Jaska-ITishnik <jasurbekbekmirzayev2004@gmail.com>
2025-01-03 17:20:26 +0500

    ci/cd example

--- root/urls.py
+++ root/urls.py
@@ -14,7 +14,7 @@ admin.site.site_header = mark_safe(
              width='60px' 
              height='60px'>
         <div style='font-family: "L ucida Handwriting", cursive; color: white;'>
-            ❄️ Fad Cargo Administration ❄️
+            ❄️ Faad Cargo Administration ❄️
         </div>
     </div>
     """
